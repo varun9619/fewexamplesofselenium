3008aea3e7b5de9d62be8bbbfc59ae24939a43a3
Varun <33460576+varun9619@users.noreply.github.com>
2019-11-07 14:02:47 +0530

    Update fb.py

--- fb.py
+++ fb.py
@@ -1,3 +1,6 @@
+''' This script opens up the fb page, login in into the application 
+using user credentials which are mentioned  '''
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
@@ -14,12 +17,11 @@ email = driver.find_element_by_xpath("//input[contains(@type,'email')]")
 email.send_keys(user)
 
 #Entering password
-pas = driver.find_element_by_id("pass")
-pas.send_keys(pwd)
+pwd = driver.find_element_by_id("pass")
+pwd.send_keys(pwd)
 
 button = driver.find_element_by_xpath("//input[contains(@value,'Log In')]")
 button.click()
-#elem.send_keys(Keys.RETURN) #clicking the button
 
 time.sleep(4) #pause the script until page loads
 
