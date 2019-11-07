e14e0abf455ca9946098a07718279cabc89a1a86
Varun <33460576+varun9619@users.noreply.github.com>
2019-11-07 14:15:37 +0530

    Added selenium weather shopper

---
+++ selenium-weather-shopper/weather-shopper.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys 
+
+driver = webdriver.Firefox() 
+driver.get('http://weathershopper.pythonanywhere.com') #opening the weather shopper app
+
+time.sleep(2)
+
+temp = driver.find_element_by_id("temperature")
+temp2 = temp.text
+print(temp2)
+temp3 = ''.join((ch if ch in '0123456789' else ' ') for ch in temp2)  
+lst = [int(i) for i in temp3.split()]
+print(temp3)
+
+temp3 = int(temp3)+0
+
+if temp3>34:
+    button=driver.find_element_by_xpath("//button[contains(.,'Buy sunscreens')]")
+    button.click()
+    print("It is hot")
+elif temp3<19:
+    button = button.find_element_by_xpath("//button[contains(.,'Buy moisturizers')]")
+    print("It is Cold")
+else:
+    print("The temperature is normal")
+
+time.sleep(1)
+driver.quit() #closing the browser
+
+
+
+
+
+ 
